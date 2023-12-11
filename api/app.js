4c501f50b0974f4f3333a526fc43fe539d1b2012
Patrick Selin <patrick.selin@gmail.com>
2023-12-11 14:17:44 +0200

    connected db client to app and ENVs

--- api/app.js
+++ api/app.js
@@ -1,7 +1,12 @@
 import { Hono } from "./deps.js";
+import { postgres } from "./deps.js";
 
 const app = new Hono();
+const sql = postgres();
 
-app.get("/", (c) => c.text("Hello World!!"));
+app.get("/", async (c) => {
+    const todos = await sql`SELECT * FROM todos`;
+    return c.json(todos);
+  });
 
 export default app;
