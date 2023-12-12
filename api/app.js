eec6b7b2ed46b54313f2f762e346eba1472ff689
Patrick Selin <patrick.selin@gmail.com>
2023-12-12 16:07:11 +0200

    feature: mainController and main.eta

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import { Hono } from "./deps.js";
 import * as todoController from "./todoController.js";
 import * as authController from "./authController.js";
+import * as mainController from "./mainController.js";
 
 const app = new Hono();
 
@@ -10,12 +11,13 @@ app.post("/auth/logout", authController.logoutUser);
 app.get("/auth/registration", authController.showRegistrationForm);
 app.post("/auth/registration", authController.registerUser);
 
-
 app.get("/todos", todoController.showForm);
 app.post("/todos", todoController.createTodo);
 
 app.get("/todos/:id", todoController.showTodo);
 app.post("/todos/:id", todoController.updateTodo);
 app.post("/todos/:id/delete", todoController.deleteTodo);
+
+app.get("/", mainController.showMain);
 
 export default app;
