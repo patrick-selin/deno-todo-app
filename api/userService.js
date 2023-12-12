294b7f075a2e8febd36d7a86f57f6a514f0ffa15
Patrick Selin <patrick.selin@gmail.com>
2023-12-12 16:09:48 +0200

    fix: deps.js and userService.js DenoKv to SQL

--- api/userService.js
+++ api/userService.js
@@ -1,12 +1,13 @@
+import { sql } from "./database.js";
+
+const createUser = async (user) => {
+  await sql`INSERT INTO users (email, password_hash) 
+    VALUES (${user.email}, ${user.passwordHash})`;
+};
+
 const findUserByEmail = async (email) => {
-    const kv = await Deno.openKv();
-    const user = await kv.get(["users", email]);
-    return user?.value;
-  };
-  
-  const createUser = async (user) => {
-    const kv = await Deno.openKv();
-    kv.set(["users", user.email], user);
-  };
-  
-  export { findUserByEmail, createUser };
+  const rows = await sql`SELECT * FROM users WHERE email = ${email}`;
+  return rows?.[0];
+};
+
+export { createUser, findUserByEmail };
